f5645d698fa0000fd30affdb972c3090bf47eece
e-python <126971921+elito-python@users.noreply.github.com>
2023-03-06 20:47:25 +0900

    Update Cisco_XR_Ver.py

--- Cisco_XR_Ver.py
+++ Cisco_XR_Ver.py
@@ -1,42 +1,57 @@
-import time
 from netmiko import ConnectHandler
+import time
 
 # デバイスへの接続情報を設定します
 device = {
-    'device_type': 'cisco_xr',　
+    'device_type': 'cisco_xr',
     'ip': 'IPアドレス', #IPアドレスを書き換えてください
-    'username': 'ユーザー名', #ユーザ名を書き換えてください
+    'username': 'ユーザ名', #ユーザ名を書き換えてください
     'password': 'パスワード', #パスワードを書き換えてください
 }
 
-# BGP経路をdenyするためのコマンドを設定します
-deny_command = 'router bgp AS番号\n neighbor IPアドレス route-policy deny_traffic out\n' #AS番号とIPアドレスを書き換えてください
+# BGP neighborを設定
+neighbor_ip = '192.168.0.2'  # BGP neighborのIPアドレスを設定
 
-# BGP経路を再度広報するためのコマンドを設定します
-allow_command = 'router bgp AS番号\n neighbor IPアドレス route-policy allow_traffic out\n'#AS番号とIPアドレスを書き換えてください
+# しきい値を設定
+threshold = 100  # トラフィックのしきい値をMbpsで設定
+recovery_threshold = 90  # トラフィックが戻ったと判定するしきい値をMbpsで設定
 
-# トラフィックレートを監視するためのインターバルを設定します（秒単位）
-monitor_interval = 5
-
-# ネットワーク上のトラフィックレートのしきい値を設定します（Mbps単位）
-traffic_threshold = 100
-
-# Netmikoを使用してデバイスに接続します
+# netmikoを使用してデバイスに接続
 with ConnectHandler(**device) as net_connect:
+    # 無限ループを開始
     while True:
-        # show interfacesコマンドを実行して、インタフェースのトラフィックレートを取得します
-        output = net_connect.send_command('show interfaces bundle-ether1 | include rate')
-        traffic_rate = int(output.split()[1]) / 1000000  # Mbps単位に変換します
-
-        # トラフィックレートがしきい値を超えた場合、BGP経路をdenyします
-        if traffic_rate > traffic_threshold:
-            net_connect.send_config_set(deny_command)
-            print(f'Traffic rate is {traffic_rate} Mbps. Denying BGP routes...')
+        # インターフェースの情報を取得
+        output = net_connect.send_command('show interface GigabitEthernet0/0/0/0')
         
-        # トラフィックレートがしきい値を下回った場合、BGP経路を再度広報します
-        elif traffic_rate <= traffic_threshold * 0.9:  # しきい値の90%以下になったらBGP広報を再開します
-            net_connect.send_config_set(allow_command)
-            print(f'Traffic rate is {traffic_rate} Mbps. Allowing BGP routes...')
+        # トラフィックの情報を取得
+        for line in output.splitlines():
+            if 'input rate' in line:
+                input_rate = int(line.split()[2])
+                break
         
-        # 監視インターバルを待ちます
-        time.sleep(monitor_interval)
+        # トラフィックがしきい値を超えた場合
+        if input_rate > threshold:
+            # 経路の広報を停止
+            config_commands = [
+                f'router bgp 65000',
+                f'neighbor {neighbor_ip} route-policy BLOCK-NEIGHBOR in',
+            ]
+            net_connect.send_config_set(config_commands)
+            print(f'{neighbor_ip}: Traffic exceeded the threshold ({input_rate} Mbps), blocking inbound routes.')
+        
+        # トラフィックが戻った場合
+        elif input_rate < recovery_threshold:
+            # 経路の広報を再開
+            config_commands = [
+                f'router bgp 65000',
+                f'no neighbor {neighbor_ip} route-policy BLOCK-NEIGHBOR in',
+            ]
+            net_connect.send_config_set(config_commands)
+            print(f'{neighbor_ip}: Traffic has returned to normal ({input_rate} Mbps), allowing inbound routes.')
+        
+        # しきい値を超えていない場合は何もしない
+        else:
+            print(f'{neighbor_ip}: Traffic is normal ({input_rate} Mbps).')
+        
+        # 10秒間待機してから再度トラフィックをチェック
+        time.sleep(10)
