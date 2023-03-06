8c9a49699eaf499b712b1bd9ebe831a9a610b2a2
e-python <126971921+elito-python@users.noreply.github.com>
2023-03-06 21:00:30 +0900

    Update Vyos_Ver.py

--- Vyos_Ver.py
+++ Vyos_Ver.py
@@ -3,43 +3,43 @@ import time
 
 # デバイスへの接続情報を設定します
 device = {
-    'device_type': 'vyos',
-    'ip': 'IPアドレス', #IPアドレスを書き換えてください
-    'username': 'ユーザ名', #ユーザ名を書き換えてください
-    'password': 'パスワード', #パスワードを書き換えてください
+    "device_type": "vyos",
+    "ip": "IPアドレス", #IPアドレスを書き換えてください
+    "username": "ユーザ名", #ユーザ名を書き換えてください
+    "password": "パスワード", #パスワードを書き換えてください
 }
 
 # インターフェース名、しきい値、およびBGPネイバーのIPアドレスを設定
-interface = '対象のPNIのIF' 
+interface = "対象のPNIのIF" 
 threshold = 100
 # BGPネイバーのIPアドレスとAS番号を設定します
-neighbor_ip = 'IPアドレス'  # BGPネイバーのIPアドレスを書き換えてください
-neighbor_as = 'AS番号'  # BGPネイバーのAS番号に置き換えてください
+neighbor_ip = "IPアドレス"  # BGPネイバーのIPアドレスを書き換えてください
+neighbor_as = "AS番号"  # BGPネイバーのAS番号に置き換えてください
 
 # 経路の広報を停止する関数
 def stop_bgp_advertising():
     net_connect = ConnectHandler(**device)
-    net_connect.send_command('configure')
-    net_connect.send_command('set protocols bgp neighbor '+ neighbor_ip +' route-map STOP_TRAFFIC out')
-    net_connect.send_command('commit')
-    net_connect.send_command('save')
-    net_connect.send_command('exit')
+    net_connect.send_command("configure")
+    net_connect.send_command("set protocols bgp neighbor "+ neighbor_ip +" route-map STOP_TRAFFIC out")
+    net_connect.send_command("commit")
+    net_connect.send_command("save")
+    net_connect.send_command("exit")
     net_connect.disconnect()
 
 # 経路の広報を再開する関数
 def start_bgp_advertising():
     net_connect = ConnectHandler(**device)
-    net_connect.send_command('configure')
-    net_connect.send_command('delete protocols bgp neighbor '+ neighbor_ip +' route-map STOP_TRAFFIC out')
-    net_connect.send_command('commit')
-    net_connect.send_command('save')
-    net_connect.send_command('exit')
+    net_connect.send_command("configure")
+    net_connect.send_command("delete protocols bgp neighbor "+ neighbor_ip +" route-map STOP_TRAFFIC out")
+    net_connect.send_command("commit")
+    net_connect.send_command("save")
+    net_connect.send_command("exit")
     net_connect.disconnect()
 
 while True:
     # トラフィック量を取得
     net_connect = ConnectHandler(**device)
-    output = net_connect.send_command('show interfaces ethernet '+ interface +' | match rate')
+    output = net_connect.send_command("show interfaces ethernet "+ interface +" | match rate")
     net_connect.disconnect()
     traffic = int(output.split()[-2])
     
