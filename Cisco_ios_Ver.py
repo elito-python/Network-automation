19e92fb5148de537be2bf72331e4b5afa5740c01
e-python <126971921+elito-python@users.noreply.github.com>
2023-03-06 21:00:46 +0900

    Update Cisco_ios_Ver.py

--- Cisco_ios_Ver.py
+++ Cisco_ios_Ver.py
@@ -3,35 +3,37 @@ import time
 
 # デバイスへの接続情報を設定します
 device = {
-    'device_type': 'cisco_ios', 
-    'ip': 'IPアドレス', #IPアドレスを書き換えてください
-    'username': 'ユーザ名', #ユーザ名を書き換えてください
-    'password': 'パスワード', #パスワードを書き換えてください
+    "device_type": "cisco_ios", 
+    "ip": "IPアドレス", #IPアドレスを書き換えてください
+    "username": "ユーザ名", #ユーザ名を書き換えてください
+    "password": "パスワード", #パスワードを書き換えてください
 }
 
 # インターフェース名、しきい値、およびBGPネイバーのIPアドレスを設定
-interface = '対象のPNIのIF' 
+interface = "対象のPNIのIF" 
 threshold = 100
-bgp_neighbor = 'IPアドレス' #IPアドレスを書き換えてください
+# BGPネイバーのIPアドレスとAS番号を設定します
+neighbor_ip = "IPアドレス"  # BGPネイバーのIPアドレスを書き換えてください
+neighbor_as = "AS番号"  # BGPネイバーのAS番号に置き換えてください
 
 # 経路の広報を停止する関数
 def stop_bgp_advertising():
     net_connect = ConnectHandler(**device)
-    net_connect.send_command('router bgp AS番号')　#AS番号を書き換えてください
-    net_connect.send_command('neighbor ' + bgp_neighbor + ' route-map STOP_ADVERTISING in')
+    net_connect.send_command("router bgp +neighbor_as")　#AS番号を書き換えてください
+    net_connect.send_command("neighbor " + neighbor_ip + " route-map STOP_ADVERTISING in")
     net_connect.disconnect()
 
 # 経路の広報を再開する関数
 def start_bgp_advertising():
     net_connect = ConnectHandler(**device)
-    net_connect.send_command('router bgp AS番号')　#AS番号を書き換えてください
-    net_connect.send_command('no neighbor ' + bgp_neighbor + ' route-map STOP_ADVERTISING in')
+    net_connect.send_command("router bgp AS番号")　#AS番号を書き換えてください
+    net_connect.send_command("no neighbor " + neighbor_ip + " route-map STOP_ADVERTISING in")
     net_connect.disconnect()
 
 while True:
     # トラフィック量を取得
     net_connect = ConnectHandler(**device)
-    output = net_connect.send_command('show interface ' + interface + ' | include rate')
+    output = net_connect.send_command("show interface " + interface + " | include rate")
     net_connect.disconnect()
     traffic = int(output.split()[-2])
     
